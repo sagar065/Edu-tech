2666a1691a05b401c9c59f6d260b4e0101d05d0d
Sagar singh <sagarbhandari243@gmail.com>
2026-07-11 15:47:06 +0530

    Enhance mailSender configuration by ensuring secure transport settings

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,9 +1,12 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const mailSender = async (email, title, body) => {
     try {
         let transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
