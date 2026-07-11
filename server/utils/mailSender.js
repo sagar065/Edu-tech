63e94cc83b2c3bc2f5d42d53a7dd271090830bbd
Sagar singh <sagarbhandari243@gmail.com>
2026-07-11 20:02:52 +0530

    Update mailSender to use environment variable for sender email and ensure transport settings are correctly configured

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -5,6 +5,8 @@ const mailSender = async (email, title, body) => {
     try {
         let transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
+            port: parseInt(process.env.MAIL_PORT),
+            secure: false,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
@@ -12,7 +14,7 @@ const mailSender = async (email, title, body) => {
         });
 
         let info = await transporter.sendMail({
-            from: `"StudyNotion || EdTech App" <${process.env.MAIL_USER}>`,
+            from: `"StudyNotion || EdTech App" <sagarbhandari243@gmail.com>`,
             to: `${email}`,
             subject: `${title}`,
             html: `${body}`,
