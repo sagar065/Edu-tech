7e6cb03b048e8ac1bba71e2f20a6e638a936614b
Sagar singh <sagarbhandari243@gmail.com>
2026-07-12 10:20:19 +0530

    Refactor email handling in mailSender and update OTP dispatch to use destructured formData in SignupForm

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,30 +1,28 @@
-const nodemailer = require("nodemailer");
+import { Resend } from 'resend';
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const mailSender = async (email, title, body) => {
-    try {
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            port: parseInt(process.env.MAIL_PORT),
-            secure: false,
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            },
-        });
+  try{
+        const { data, error } = await resend.emails.send({
+        from: ' "StudyNotion || Sagar Singh" <onboarding@resend.dev> ',
+        to: email,
+        subject: title,
+        html: body,
+      });
 
-        let info = await transporter.sendMail({
-            from: `"StudyNotion || EdTech App" <sagarbhandari243@gmail.com>`,
-            to: `${email}`,
-            subject: `${title}`,
-            html: `${body}`,
-        });
+        if (error) {
+            console.error({ error });
+            return null;
+        }
 
-        console.log("Email info: ", info);
-        return info;
-    } catch (error) {
-        console.log("Error in mailSender: ", error.message);
-        throw error;
+        console.log({ data });
+        return data;
+    }
+  
+    catch(error){ 
+    console.error({ "Mail Sending Error": error });
+    return null;
     }
 };
 
