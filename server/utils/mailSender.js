93b042d384358e73f918f4f86f5bf7a0949b4601
Sagar singh <sagarbhandari243@gmail.com>
2026-07-12 11:31:07 +0530

    Refactor mailSender to use nodemailer instead of Resend for email handling

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,29 +1,31 @@
-const { Resend } =  require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
-  try{
-        const { data, error } = await resend.emails.send({
-        from: ' "StudyNotion || Sagar Singh" <onboarding@resend.dev> ',
-        to: email,
-        subject: title,
-        html: body,
-      });
+    try{
+            let transporter = nodemailer.createTransport({
+                host:process.env.MAIL_HOST,
+                secure: false,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                },
+            });
 
-        if (error) {
-            console.error({ error });
-            return null;
-        }
 
-        console.log({ data });
-        return data;
+            let info = await transporter.sendMail({
+                from: 'StudyNotion || CodeHelp - by Babbar',
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            console.log(info);
+            return info;
     }
-  
-    catch(error){ 
-        console.error({ "Mail Sending Error": error });
-        return null;
+    catch(error) {
+        console.log(error.message);
+        throw error;
     }
-};
+}
+
 
 module.exports = mailSender;
