8442c10724cc381b555a03b2cc0ce2708efb1fec
Sagar singh <sagarbhandari243@gmail.com>
2026-07-13 11:57:12 +0530

    Fix variable name for email template in OTP model

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
-const emailTemplate = require("../mail/templates/emailVerificationTemplate");
+const otpTemplate = require("../mail/templates/emailVerificationTemplate");
 
 const OTPSchema = new mongoose.Schema({
     email: {
@@ -24,7 +24,7 @@ async function sendVerificationEmail(email, otp) {
         const mailResponse = await mailSender(
             email,
             "Verification Email from StudyNotion",
-            emailTemplate(otp)
+            otpTemplate(otp)
         );
         console.log("Email sent successfully: ", mailResponse.response);
     } catch (error) {
